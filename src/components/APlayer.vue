dafb9753bee9095c0b5b59426e8604fa669e4295
BlueCHN <181114865+BlueCHN@users.noreply.github.com>
2025-04-13 22:04:57 +0800

    refactor-音乐组件可用，注意音频存放位置应为public/music中

--- src/components/APlayer.vue
+++ src/components/APlayer.vue
@@ -6,15 +6,18 @@
 </template>
 
 <script>
-import 'APlayer/dist/APlayer.min.css';
-import APlayer from 'Aplayer';
+import 'aplayer/dist/APlayer.min.css';
+import APlayer from 'aplayer';
 
 export default {
     data(){
         return {
             player:null,
             audios: [
-                { name: 'Song1', url: '../assets/网设BGM.mp3'},
+                {   name: 'Song1',
+                    url: '/music/BGM.mp3',
+                    // cover: ''
+                },
             ]
         };
     },
@@ -23,7 +26,11 @@ export default {
         this.player = new APlayer({
             fixed: true,
             container: document.getElementById('aplayer-container'),
-            audio: this.audios
+            audio: this.audios,
+            autoplay: true,
+            preload: 'auto',
+            muted: true,  // 静音状态下绕过限制
+            volume: 0.5 
         });
     },
 
